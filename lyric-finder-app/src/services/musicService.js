216e1a26cde1fe15b6aa496adde92e40fe79ca5d
Paul Schwan <paulschwan@MacBook-Pro-3.local>
2024-11-26 21:08:17 +0000

    fixing getting song list

--- lyric-finder-app/src/services/musicService.js
+++ lyric-finder-app/src/services/musicService.js
@@ -1,59 +1,89 @@
-import { Buffer } from 'buffer';
+import {Buffer} from 'buffer';
 
 const baseUrl = 'https://api.spotify.com/v1/';
 const loginUrl = 'https://accounts.spotify.com/api/token';
 
 export const getArtistId = async (artistName) => {
-	var authToken = await getAuthToken();
-	let artistIdResponse = await fetch(
-		`${baseUrl}search?q=${artistName}&type=artist`,
-		{
-			method: 'GET',
-			headers: {
-				Authorization: `Bearer ${authToken}`,
-			},
-		}
-	);
+    var authToken = await getAuthToken();
+    let artistIdResponse = await fetch(
+        `${baseUrl}search?q=${artistName}&type=artist`,
+        {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${authToken}`,
+            },
+        }
+    );
 
-	let data = await artistIdResponse.json();
-	console.log('Data returned from searching for artist: ');
-	console.log(data);
+    let data = await artistIdResponse.json();
+    console.log('Data returned from searching for artist: ');
+    console.log(data);
 
-	if (data.artists != null) {
-		console.log('returning ' + data.artists.items[0].id);
-		return data.artists.items[0].id;
-	}
+    if (data.artists != null) {
+        console.log('returning ' + data.artists.items[0].id);
+        return data.artists.items[0].id;
+    }
 };
 
-export const getArtistSongs = async (artistID) => {
-	var authToken = await getAuthToken();
-	let artistDetailsResponse = await fetch(
-		`${baseUrl}artists/${artistID}/top-tracks`,
-		{
-			method: 'GET',
-			headers: {
-				Authorization: `Bearer ${authToken}`,
-			},
-		}
-	);
+export function getArtistSongs(artistID) {
+    console.log("getting artist songs");
+    return getAuthToken()
+        .then((authToken) => {
+            return fetch(
+                `${baseUrl}artists/${artistID}/top-tracks`,
+                {
+                    method: 'GET',
+                    headers: {
+                        Authorization: `Bearer ${authToken}`,
+                    },
+                })
+                .then((response) => {
+                    return response.json()
+                })
+                .then((data) => {
+                    console.log("song list data:");
+                    console.log(data);
+                    let trackList = [];
+                    data.tracks.forEach((track) => {
+                        trackList.push({id: track.id, name: track.name});
+                    });
+                    console.log('track list: ');
+                    console.log(trackList);
+                    return trackList;
+                })
+        });
+}
 
-	let data = await artistDetailsResponse.json();
-	console.log('Data returned from searching for songs for artist: ');
-	console.log(data);
+function getAuthToken() {
+    // TODO: add code here to check if we have an unexpired token already, before getting a fresh one
 
-	if (data.tracks != null) {
-		let trackList = [];
-		data.tracks.forEach((track) => {
-			trackList.push({ id: track.id, name: track.name });
-		});
-		console.log('track list: ');
-		console.log(trackList);
-		return trackList;
-	}
-};
+    console.log('getting fresh auth token');
+
+    var client_id = '2eb5efccbdd64e9daca506b2a9de7e9e';
+    var client_secret = '7f1a0d75767441f7bc7c83bb319d00ea';
+
+    return fetch(loginUrl, {
+        method: 'POST',
+        headers: {
+            Authorization:
+                'Basic ' +
+                new Buffer.from(client_id + ':' + client_secret).toString(
+                    'base64'
+                ),
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: 'grant_type=client_credentials',
+        json: true,
+    }).then(response => {
+        return response.json();
+    }).then(data => {
+        return data.access_token;
+    })
+}
+
 
 export const getLyrics = (songName) => {
-	return `It's all about you (it's about you)
+    return `It's all about you (it's about you)
 It's all about you, baby (it's all about)
 It's all about you (it's about you)
 It's all about you
@@ -86,39 +116,4 @@ It's all about you, baby (it's all about you)
 It's all about you (it's about you)
 It's all about you
 It's all about you`;
-};
-
-export const getAuthToken = async () => {
-	// TODO: add code here to check if we have an unexpired token already, before getting a fresh one
-
-	console.log('getting fresh auth token');
-
-	var client_id = '2eb5efccbdd64e9daca506b2a9de7e9e';
-	var client_secret = '7f1a0d75767441f7bc7c83bb319d00ea';
-
-	let authTokenResponse = await fetch(loginUrl, {
-		method: 'POST',
-		headers: {
-			Authorization:
-				'Basic ' +
-				new Buffer.from(client_id + ':' + client_secret).toString(
-					'base64'
-				),
-			'Content-Type': 'application/x-www-form-urlencoded',
-		},
-		body: 'grant_type=client_credentials',
-		json: true,
-	});
-
-	let successfulLogin = authTokenResponse.status;
-	if (successfulLogin && !authTokenResponse.ok) {
-		console.log(
-			"got the successful login, but the auth token response wasn't ok: " +
-				authTokenResponse.status
-		);
-		return null;
-	}
-	let data = await authTokenResponse.json();
-	window.localStorage.setItem('token', data.access_token);
-	return data.access_token;
 };
